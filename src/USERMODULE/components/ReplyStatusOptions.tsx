91e93c0907114fb387d972f4314139fcf22661d6
deepakMS12 <deepak.dhiman@mscorpres.in>
2025-09-03 17:16:25 +0530

    api integrate in status tab component

---
+++ src/USERMODULE/components/ReplyStatusOptions.tsx
@@ -0,0 +1,35 @@
+import { MenuItem } from "@mui/material";
+
+const options = [
+  {
+    label: "Send and set as Pending",
+    value: "PEN",
+  },
+  {
+    label: "Send and set as Resolved",
+    value: "RES",
+  },
+  {
+    label: "Send and set as Closed",
+    value: "CLD",
+  },
+];
+const ReplyStatusOptions = ({ onSetData }: any) => {
+  return (
+    <div className="w-full p-3">
+      {options.map((item: any) => (
+        <MenuItem
+          key={item?.value}
+          value={item?.value}
+          onClick={() => {
+            onSetData(item?.value);
+          }}
+        >
+          {item?.label}
+        </MenuItem>
+      ))}
+    </div>
+  );
+};
+
+export default ReplyStatusOptions;
