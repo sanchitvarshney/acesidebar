38f5c2c387acb01384ce5eba14f2ce61b6eca997
Shiv Kumar <shiv.kumar@mscorpres.in>
2025-09-21 20:44:57 +0530

    class added

--- src/USERMODULE/pages/TicketManagement/Tickets.tsx
+++ src/USERMODULE/pages/TicketManagement/Tickets.tsx
@@ -792,7 +792,7 @@ const Tickets: React.FC = () => {
               strokeWidth="1.5"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="lucide lucide-user-round-check text-gray-500 mt-0.5"
+              className="lucide text-gray-500 mt-0.5"
             >
               <path d="M2 21a8 8 0 0 1 13.292-6" />
               <circle cx="10" cy="8" r="5" />
@@ -814,7 +814,7 @@ const Tickets: React.FC = () => {
           {/* Raised by */}
 
           <div className="flex items-start gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user-star-icon lucide-user-star"><path d="M16.051 12.616a1 1 0 0 1 1.909.024l.737 1.452a1 1 0 0 0 .737.535l1.634.256a1 1 0 0 1 .588 1.806l-1.172 1.168a1 1 0 0 0-.282.866l.259 1.613a1 1 0 0 1-1.541 1.134l-1.465-.75a1 1 0 0 0-.912 0l-1.465.75a1 1 0 0 1-1.539-1.133l.258-1.613a1 1 0 0 0-.282-.866l-1.156-1.153a1 1 0 0 1 .572-1.822l1.633-.256a1 1 0 0 0 .737-.535z" /><path d="M8 15H7a4 4 0 0 0-4 4v2" /><circle cx="10" cy="7" r="4" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide text-gray-500 mt-0.5"><path d="M16.051 12.616a1 1 0 0 1 1.909.024l.737 1.452a1 1 0 0 0 .737.535l1.634.256a1 1 0 0 1 .588 1.806l-1.172 1.168a1 1 0 0 0-.282.866l.259 1.613a1 1 0 0 1-1.541 1.134l-1.465-.75a1 1 0 0 0-.912 0l-1.465.75a1 1 0 0 1-1.539-1.133l.258-1.613a1 1 0 0 0-.282-.866l-1.156-1.153a1 1 0 0 1 .572-1.822l1.633-.256a1 1 0 0 0 .737-.535z" /><path d="M8 15H7a4 4 0 0 0-4 4v2" /><circle cx="10" cy="7" r="4" /></svg>
 
             <div className="flex flex-col">
               <span className="text-gray-500 mb-1">raised by</span>
@@ -847,7 +847,7 @@ const Tickets: React.FC = () => {
               strokeWidth="1.5"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="lucide lucide-building text-gray-500 mt-0.5"
+              className="lucide text-gray-500 mt-0.5"
             >
               <path d="M12 10h.01" />
               <path d="M12 14h.01" />
@@ -886,7 +886,7 @@ const Tickets: React.FC = () => {
               strokeWidth="1.5"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="lucide lucide-trending-up text-gray-500 mt-0.5"
+              className="lucide text-gray-500 mt-0.5"
             >
               <path d="M16 7h6v6" />
               <path d="m22 7-8.5 8.5-5-5L2 17" />
@@ -916,7 +916,7 @@ const Tickets: React.FC = () => {
               strokeWidth="1.5"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="lucide lucide-calendar-clock text-gray-500 mt-0.5"
+              className="lucide text-gray-500 mt-0.5"
             >
               <path d="M16 14v2.2l1.6 1" />
               <path d="M16 2v4" />
