1c1ae9e56439a4fbb717665efc45cfcc02124f11
Shiv Kumar <shiv.kumar@mscorpres.in>
2025-09-23 11:47:30 +0530

    border color

--- src/USERMODULE/pages/TicketManagement/Tickets.tsx
+++ src/USERMODULE/pages/TicketManagement/Tickets.tsx
@@ -672,7 +672,7 @@ const Tickets: React.FC = () => {
     return (
       <div
         key={merged?.ticketNumber}
-        className="bg-white border-2 border-[#e1e7ee] rounded-xl mb-4 p-4 hover:shadow-lg transition-shadow duration-200 cursor-pointer relative"
+        className="bg-white border-2 border-[#d4e6ff] rounded-xl mb-4 p-4 hover:shadow-lg transition-shadow duration-200 cursor-pointer relative"
         onClick={() => handleTicketSubjectClick(merged.ticketNumber)}
       >
         {/* Top section */}
